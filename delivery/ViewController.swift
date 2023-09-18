648c6f644822d4bf11892026761d63c46921f90b
aswin <aswin0484@gmail.com>
2023-09-18 14:16:46 +0530

    initial commit

--- delivery/ViewController.swift
+++ delivery/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+   
+    var txtlab = ["Biriyani","Nan","Meals","Pizza", "Chappathi"]
+    var inpimg = ["1","2","3","4","5"]
 
+    @IBOutlet weak var collection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collection.delegate = self
+        collection.dataSource = self
+        
+        let layout = UICollectionViewFlowLayout()
+        collection.collectionViewLayout = layout
+        layout.scrollDirection = .horizontal
+        
         // Do any additional setup after loading the view.
     }
 
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return txtlab.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collection.dequeueReusableCell(withReuseIdentifier: "itemCell", for: indexPath) as! itemsCollectionViewCell
+        cell.img.image = UIImage(named: inpimg[indexPath.item])
+        cell.lab.text = txtlab[indexPath.item]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height/4)
+    }
 
 }
 
---
+++ delivery/itemsCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  itemsCollectionViewCell.swift
+//  delivery
+//
+//  Created by ASWIN A on 18/09/23.
+//
+
+import UIKit
+
+class itemsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    
+    @IBOutlet weak var lab: UILabel!
+    
+}
